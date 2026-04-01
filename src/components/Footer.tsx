1efeea1ca7454439a9f9246e75311ae77b0f04df
C-Friberg <you@example.com>
2026-04-01 17:24:37 +0200

    basic styling, added content to landing page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <p>© 2026 FoodSwipe</p>
-            <p>Vanliga frågor, se vår <a href= "www.google.com" target="_blank">FAQ</a></p>
+            <p>Vanliga frågor, se vår <a href= "www.google.com" target="_blank">FAQ</a></p> {/* Leder ingenstans just nu */}
         </footer>
     )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,8 @@ const HomePage = () => {
     <div>
         <h1>FoodSwipe</h1>
         <h2>Upptäck ditt nästa favoritrecept</h2>
+        <p>Ladda ner appen nu på App Store eller Google Play</p>
+        <img src="/src/assets/demo.png" className = "demo-img" ></img>
     </div>
 )
 }
